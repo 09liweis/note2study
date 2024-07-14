41cfffeef9ed079f1026dcb32a9782ce652244ca
Sam Li <weisen.li@hotmail.com>
2024-07-14 23:03:22 +0000

    remove scrollview on notes page

--- app/(tabs)/(note)/notes.tsx
+++ app/(tabs)/(note)/notes.tsx
@@ -1,7 +1,5 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { StyleSheet, Image, Platform, FlatList } from "react-native";
 
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { NoteCard } from "@/components/notes/NoteCard";
@@ -28,13 +26,8 @@ export default function TabTwoScreen() {
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
+    <ThemedView style={styles.notesContainer}>
+      <ThemedView>
         <ThemedText type="title">Your notes to study</ThemedText>
       </ThemedView>
       <ThemedButton
@@ -43,23 +36,18 @@ export default function TabTwoScreen() {
         onPress={handleAddBtnClick}
       />
       <FlatList
+        contentContainerStyle={{}}
         data={notes}
         renderItem={({ item }) => <NoteCard note={item} />}
       />
-    </ParallaxScrollView>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
+  notesContainer: {
+    padding: 32,
+    paddingBottom: 100,
   },
   addBtn: {},
 });
